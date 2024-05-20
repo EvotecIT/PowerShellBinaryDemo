526d742834bb791ef51f57f57857a1d70e618665
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-20 09:46:59 +0200

    improve example

--- Module/Examples/Test.ps1
+++ Module/Examples/Test.ps1
@@ -1,3 +1,9 @@
 ﻿Import-Module .\Module\PowerShellBinaryDemo.psd1 -Force
 
+Write-Host -ForegroundColor Green "Testing Test-PowerShellBinary"
+
 Test-PowerShellBinary -Verbose
+
+Write-Host -ForegroundColor Green "Testing Test-PowerShellBinaryStatic"
+
+Test-PowerShellBinaryStatic -Verbose
