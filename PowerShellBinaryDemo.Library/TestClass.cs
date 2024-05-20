3ea2a90083b593783c08cda173b504b1221d0d27
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-20 08:45:35 +0200

    Working module

--- PowerShellBinaryDemo.Library/TestClass.cs
+++ PowerShellBinaryDemo.Library/TestClass.cs
@@ -1,8 +1,16 @@
 ﻿namespace PowerShellBinaryDemo.Library;
 
-public class TestClass {
-    public TestClass() { }
+public class TestClass : Settings {
+    public TestClass(InternalLogger? internalLogger = null) {
+        if (internalLogger != null) {
+            _logger = internalLogger;
+        }
+    }
     public string TestMethod() {
+        _logger.WriteVerbose("Test Verbose Message from inside the library");
+        _logger.WriteWarning("Test Warning Message from inside the library");
+        _logger.WriteDebug("Test Debug Message from inside the library");
+        _logger.WriteError("Test Error Message from inside the library");
         return "Hello World!";
     }
 }
--- PowerShellBinaryDemo/CmdletTestPowerShellBinary.cs
+++ PowerShellBinaryDemo/CmdletTestPowerShellBinary.cs
@@ -1,33 +1,39 @@
 ﻿using System.Management.Automation;
 using System.Threading.Tasks;
-
 using PowerShellBinaryDemo.Library;
 
 namespace PowerShellBinaryDemo;
 
 [Cmdlet(VerbsDiagnostic.Test, "PowerShellBinary")]
-public sealed class CmdletWriteWinEvent : AsyncPSCmdlet {
-
+public sealed class CmdletTestPowerShellBinary : AsyncPSCmdlet {
+    // let's create a logger so we can write messages to the console
     private InternalLogger InternalLogger;
+    // let's create a TestClass so we can call the TestMethod from different sections
     private TestClass TestClass;
     protected override Task BeginProcessingAsync() {
         // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
         InternalLogger = new InternalLogger(false);
         var internalLoggerPowerShell = new InternalLoggerPowerShell(InternalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+
+        // Write a verbose message to the console.
         InternalLogger.WriteVerbose("Test Begin Section");
 
-        TestClass = new TestClass();
+        // Initialize the TestClass to be able to call the TestMethod.
+        TestClass = new TestClass(InternalLogger);
 
         return Task.CompletedTask;
     }
     protected override Task ProcessRecordAsync() {
+        // Write a verbose message to the console.
         InternalLogger.WriteVerbose("Test Processing Section");
 
+        // Write the result of the TestMethod to the console.
         WriteObject(TestClass.TestMethod());
 
         return Task.CompletedTask;
     }
     protected override Task EndProcessingAsync() {
+        // Write a warning message to the console.
         InternalLogger.WriteWarning("Test Warning in End Section");
         return Task.CompletedTask;
     }
