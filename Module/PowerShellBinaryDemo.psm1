5dc189947ee0dcd30919070edd1c54e2f98f0e85
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-05-20 09:17:39 +0200

    Improvements

--- Module/PowerShellBinaryDemo.psm1
+++ Module/PowerShellBinaryDemo.psm1
@@ -1,4 +1,13 @@
-﻿# Get public and private function definition files.
+﻿# to speed up development adding direct path to binaries, instead of the the Lib folder
+$Development = $true
+$DevelopmentPath = "$PSScriptRoot\..\PowerShellBinaryDemo\bin\Debug"
+$DevelopmentFolderCore = "net7.0"
+$DevelopmentFolderDefault = "net472"
+$BinaryModules = @(
+    "PowerShellBinaryDemo.dll"
+)
+
+# Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse -File)
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse -File)
 $Classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue -Recurse -File)
@@ -6,81 +15,74 @@ $Enums = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1 -ErrorAction SilentlyC
 # Get all assemblies
 $AssemblyFolders = Get-ChildItem -Path $PSScriptRoot\Lib -Directory -ErrorAction SilentlyContinue -File
 
-# to speed up development adding direct path to binaries, instead of the the Lib folder
-$Development = $true
-$DevelopmentPath = "$PSScriptRoot\..\PowerShellBinaryDemo\bin\Debug"
-$DevelopmentFolderCore = "netstandard2.0"
-$DevelopmentFolderDefault = "net472"
-$BinaryModules = @(
-    "PowerShellBinaryDemo.dll"
-)
-
 # Lets find which libraries we need to load
-$Default = $false
-$Core = $false
-$Standard = $false
-foreach ($A in $AssemblyFolders.Name) {
-    if ($A -eq 'Default') {
-        $Default = $true
-    } elseif ($A -eq 'Core') {
-        $Core = $true
-    } elseif ($A -eq 'Standard') {
-        $Standard = $true
-    }
-}
-if ($Standard -and $Core -and $Default) {
-    $FrameworkNet = 'Default'
-    $Framework = 'Standard'
-} elseif ($Standard -and $Core) {
-    $Framework = 'Standard'
-    $FrameworkNet = 'Standard'
-} elseif ($Core -and $Default) {
+if ($Development) {
     $Framework = 'Core'
-    $FrameworkNet = 'Default'
-} elseif ($Standard -and $Default) {
-    $Framework = 'Standard'
-    $FrameworkNet = 'Default'
-} elseif ($Standard) {
-    $Framework = 'Standard'
-    $FrameworkNet = 'Standard'
-} elseif ($Core) {
-    $Framework = 'Core'
-    $FrameworkNet = ''
-} elseif ($Default) {
-    $Framework = ''
     $FrameworkNet = 'Default'
 } else {
-    #Write-Error -Message 'No assemblies found'
+    $Default = $false
+    $Core = $false
+    $Standard = $false
+    foreach ($A in $AssemblyFolders.Name) {
+        if ($A -eq 'Default') {
+            $Default = $true
+        } elseif ($A -eq 'Core') {
+            $Core = $true
+        } elseif ($A -eq 'Standard') {
+            $Standard = $true
+        }
+    }
+    if ($Standard -and $Core -and $Default) {
+        $FrameworkNet = 'Default'
+        $Framework = 'Standard'
+    } elseif ($Standard -and $Core) {
+        $Framework = 'Standard'
+        $FrameworkNet = 'Standard'
+    } elseif ($Core -and $Default) {
+        $Framework = 'Core'
+        $FrameworkNet = 'Default'
+    } elseif ($Standard -and $Default) {
+        $Framework = 'Standard'
+        $FrameworkNet = 'Default'
+    } elseif ($Standard) {
+        $Framework = 'Standard'
+        $FrameworkNet = 'Standard'
+    } elseif ($Core) {
+        $Framework = 'Core'
+        $FrameworkNet = ''
+    } elseif ($Default) {
+        $Framework = ''
+        $FrameworkNet = 'Default'
+    }
 }
 
-$Assembly = @(
-    if ($Development) {
+
+$BinaryDev = @(
+    foreach ($BinaryModule in $BinaryModules) {
         if ($PSEdition -eq 'Core') {
-            Get-ChildItem -Path $DevelopmentPath\$DevelopmentFolderCore\*.dll -ErrorAction SilentlyContinue -Recurse
+            $Variable = Resolve-Path "$DevelopmentPath\$DevelopmentFolderCore\$BinaryModule"
+            $DevelopmentAssemblyFolder = Resolve-Path "$DevelopmentPath\$DevelopmentFolderCore"
         } else {
-            Get-ChildItem -Path $DevelopmentPath\$DevelopmentFolderDefault\*.dll -ErrorAction SilentlyContinue -Recurse
+            $Variable = Resolve-Path "$DevelopmentPath\$DevelopmentFolderDefault\$BinaryModule"
+            $DevelopmentAssemblyFolder = Resolve-Path "$DevelopmentPath\$DevelopmentFolderDefault"
         }
-    } else {
+        $Variable
+        Write-Warning "Development mode: Using binaries from $Variable"
+    }
+)
+
+if ($Development) {
+    $Assembly = Get-ChildItem -Path "$($DevelopmentAssemblyFolder.Path)\*.dll" -ErrorAction SilentlyContinue -File
+} else {
+    $Assembly = @(
         if ($Framework -and $PSEdition -eq 'Core') {
             Get-ChildItem -Path $PSScriptRoot\Lib\$Framework\*.dll -ErrorAction SilentlyContinue -Recurse
         }
         if ($FrameworkNet -and $PSEdition -ne 'Core') {
             Get-ChildItem -Path $PSScriptRoot\Lib\$FrameworkNet\*.dll -ErrorAction SilentlyContinue -Recurse
         }
-    }
-)
-
-$BinaryDev = @(
-    foreach ($BinaryModule in $BinaryModules) {
-        if ($PSEdition -eq 'Core') {
-            $Variable = Resolve-Path "$DevelopmentPath\$DevelopmentFolderCore\$BinaryModule"
-        } else {
-            $Variable = Resolve-Path "$DevelopmentPath\$DevelopmentFolderDefault\$BinaryModule"
-        }
-        $Variable
-        Write-Warning "Development mode: Using binaries from $Variable"
-    }
-)
+    )
+}
 
 $FoundErrors = @(
     if ($Development) {
@@ -109,9 +111,8 @@ $FoundErrors = @(
     }
     Foreach ($Import in @($Assembly)) {
         try {
-            Write-Verbose -Message $Import.FullName
+            Write-Warning -Message $Import.FullName
             Add-Type -Path $Import.Fullname -ErrorAction Stop
-            #  }
         } catch [System.Reflection.ReflectionTypeLoadException] {
             Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
             $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
